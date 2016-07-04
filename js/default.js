83d0c69c54b5d0df5c33a16ac82f8e8c1ecebe26
yumapiero <g1527046@st.t-kougei.ac.jp>
2016-07-04 11:41:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var thmubs = document.querySelectorAll('.thumb');
 for(idx in thmubs){
     thmubs[idx].onclick = function(){
-        document.getElementById("bigimg").src = 'img/' + this.dataset.image + '.jpg';
+        document.getElementById("bigimg").src = 'img/' + this.dataset.image + '.png';
     }
 }
 
